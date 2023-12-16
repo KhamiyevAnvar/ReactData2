460537bef6df47dff3d453d73f75a753561f6fdd
Anvar Khamiyev <54187515+KhamiyevAnvar@users.noreply.github.com>
2023-12-16 23:38:51 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Head1 } from "./style/styledComp.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Head1>Salam</Head1>
+      <div>{process.env.NODE_ENV}</div>
+    </>
   );
 }
 
---
+++ src/style/styledComp.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Head1 = styled.h1`
+  color: red;
+  transition: ease all 0.4s;
+  &:hover {
+    color: blue;
+  }
+`;
